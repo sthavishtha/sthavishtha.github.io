ef053b8c396a784cfd074cd1774676415437e3a0
sthavishtha <sbhopla@caltech.edu>
2026-08-14 20:40:30 +0000

    Deploying to gh-pages from @ sthavishtha/sthavishtha.github.io@5587418fc4c01c834bcf142514c82ed3d5bd49e1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,7 +407,7 @@ ninja.data = [{
               window.location.href = "/projects/1_computational_mechanics/";
             },},{id: "projects-mechanics-of-multiphase-flows",
           title: 'Mechanics of multiphase flows',
-          description: "droplet dynamics, evaporation, pinch-off, and transport in porous and fractured media",
+          description: "pore-scale physics of sea ice and dry snow, resolved with phase-field simulations",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_multiphase_flows/";
             },},{id: "projects-mechanics-of-multi-component-fluid-structure-interaction",
